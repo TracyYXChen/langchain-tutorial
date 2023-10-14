3c810a31e7728c6a8f206761665abcb996c13c29
Tracy Chen <ychen151@umd.edu>
2023-10-14 17:40:12 -0400

    render prompt

--- openai/src/App.tsx
+++ openai/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
-import { socks } from './components/socks';
-import { useEffect } from 'react';
+import { turtle } from './components/turtle';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState<string | null>(null); // Using null as initial state
+  const [error, setError] = useState<Error | null>(null); // To handle any potential error
+  const prompt = "Name a turtle stands in front of a library";
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await socks();
+        const turtleName = await turtle(prompt);
+        setData(turtleName);
       } catch (error) {
         console.error("An error occurred:", error);
       }
@@ -16,6 +20,18 @@ function App() {
   }, []);
   return (
     <div className="App">
+      {error && <p>Error: {error.message}</p>}
+      {data ? (
+        <div>
+        <p> {prompt} </p>
+        <b> {data}</b>
+        </div>
+      ) : (
+        <div>
+        <p> {prompt} </p>
+        <b> loading ...</b>
+        </div>
+      )}
     </div>
   );
 }
